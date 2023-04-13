db7629623dd41f5c504612b8ccc17e1b298b8f8b
Jan-Luca Laubner <j.laubner@ostfalia.de>
2023-04-13 12:33:00 +0200

    dded private and public attribute (correction #1)

--- src/vehicle.cc
+++ src/vehicle.cc
@@ -1,16 +1,22 @@
 #include "vehicle.h"
 #include <iostream>
+#include <string>
 
- void Vehicle::read() {
-            std::cout << "Brand: " << std::endl;
-            std::cin >> brand;
-            std::cout << "Model: " << std::endl;
-            std::cin >> model;
-            std::cout << "Year: " << std::endl;
-            std::cin >> year;
-            };
-void Vehicle::print(){
-            std::cout << "Brand: " << brand << std::endl;
-    	    std::cout << "Model: " << model << std::endl;
-            std::cout << "Year: " << year << std::endl;
-        }
+void Vehicle::read() 
+{
+    std::cout << "Brand: " << std::endl;
+    std::getline(std::cin, brand);
+         
+    std::cout << "Model: " << std::endl;
+    std::getline(std::cin, model);
+
+    std::cout << "Year: " << std::endl;
+    std::cin >> year;
+}
+
+void Vehicle::print()
+ {
+    std::cout << "\tbrand=" << brand << std::endl;
+    std::cout << "\tmodel=" << model << std::endl;
+    std::cout << "\tyear=" << year << std::endl;
+}
